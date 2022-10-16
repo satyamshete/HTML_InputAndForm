bfb578c9e3c7d943dfa90b3c2258cb93a37bc17d
satyamshete <shetesatyam99@gmail.com>
2022-10-16 22:54:01 +0530

    [Satyam] Input_Form_Email_Validation

--- InputAndForm/js/validation.js
+++ InputAndForm/js/validation.js
@@ -23,3 +23,19 @@ text.addEventListener('input',function()
     Output.textContent=salaryInput.value
  }
  )
+
+
+  var Email = document.querySelector("#email")
+  var EmailError = document.querySelector(".error-email")
+Email.addEventListener('input',function()
+{
+  email = RegExp("^[a-z A-Z 0-9 .+_-]+@[a-z A-Z 0-9 -.]+[.][a-z A-Z 0-9 -]{2,}$");
+  if (email.test(Email.value))
+  {
+   EmailError.textContent =""
+  }
+  else
+  {
+   EmailError.textContent ="Invalid Email"
+  }
+})
